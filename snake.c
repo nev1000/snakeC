01e1f84e69493384f134e8abb5292b7c12e79a2e
nev1000 <jnevell100@gmail.com>
2026-08-18 16:59:45 -0400

    new version

--- snake.c
+++ snake.c
@@ -1,116 +1,200 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <stdbool.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <time.h>
+    #include <stdbool.h>
+    #include <unistd.h>
+    #include <ncurses.h>
 
-int SIZE = 20;
+    int SIZE = 20;
 
-typedef struct {
-    int x;
-    int y;
-} tuple;
+    typedef struct {
+        int x;
+        int y;
+    } tuple;
 
-typedef struct {
-    tuple positions[20 * 20];
-    int length;
-} snake;
+    typedef struct {
+        tuple positions[20 * 20];
+        int length;
+    } snake;
 
-void print_grid(tuple pellet_location, tuple snake_location[]);
-tuple get_rand_number();
-bool check_snake(int pos_x, int pos_y, tuple positions[]);
+    void print_grid(tuple pellet_location, tuple snake_location[]);
+    tuple get_rand_number();
+    bool check_snake(int pos_x, int pos_y, tuple positions[]);
+    void update_snake(snake* snakey, char direction);
+    bool check_collision(tuple pellet_location, tuple snakey_head);
 
-int main (void){    
-    tuple location = get_rand_number();
-    tuple snake_starting_position = {1, 1};
-    tuple snake_starting_position_1 = {2, 2};
-    snake snakey;
-    snakey.length = 0;
-    for (int i = 0 ; i < 400 ; i++){
-        snakey.positions[i].x = 0;
-        snakey.positions[i].y = 0;
-    }
-    snakey.positions[0] = snake_starting_position;
-    snakey.positions[1] = snake_starting_position_1;
-    snakey.length = 2;
-    print_grid(location, snakey.positions);
-    char input;
-    scanf("%c", &input);
-    if (input == 'd'){
+
+    int main (void){   
+         // ---- ncurses setup: this is what was missing ----
+    initscr();              // take over the terminal; REQUIRED before any getch()
+    cbreak();               // deliver keys immediately, no waiting for Enter
+    noecho();               // don't print the keys the user presses
+    keypad(stdscr, TRUE);   // makes KEY_RIGHT etc. work as single values
+    curs_set(0);            // hide the blinking cursor
+    timeout(50);           // getch() waits at most 500 ms -> replaces usleep()
+    // ----- 
+        char direc = 'd';
+        tuple location = get_rand_number();
+        tuple snake_starting_position = {1, 1};
+        tuple snake_starting_position_1 = {2, 2};
+        tuple snake_starting_position_2 = {3, 2};
+        snake snakey;
+        snakey.length = 0;
         for (int i = 0 ; i < 400 ; i++){
-            if (snakey.positions[i].x == 0 && snakey.positions[i].y == 0){
-                snakey.positions[i].x = snakey.positions[i - 1].x + 1;
-                snakey.positions[i].y = snakey.positions[i - 1].y;
-                snakey.positions[0].x = -1;
-                snakey.positions[0].y = -1;
-                break;
-            }
+            snakey.positions[i].x = 0;
+            snakey.positions[i].y = 0;
+        }
+        snakey.positions[0] = snake_starting_position;
+        snakey.positions[1] = snake_starting_position_1;
+        snakey.positions[2] = snake_starting_position_2;
+        snakey.length = 3;
+        // print_grid(location, snakey.positions);
+        // char input;
+        // scanf("%c", &input);
+        // if (input == 'd'){
+        //     for (int i = 0 ; i < 400 ; i++){
+        //         if (snakey.positions[i].x == 0 && snakey.positions[i].y == 0){
+        //             snakey.positions[i].x = snakey.positions[i - 1].x + 1;
+        //             snakey.positions[i].y = snakey.positions[i - 1].y;
+        //             snakey.positions[0].x = -1;
+        //             snakey.positions[0].y = -1;
+        //             break;
+        //         }
+        //     }
+        // }
+        while (true){
+            int ch = getch();   // ← this line is the "background listening"
+            if (ch == 'd' || ch == KEY_DOWN) direc = 'd';
+            if (ch == 'u' || ch == KEY_UP) direc = 'u';
+            if (ch == 'l' || ch == KEY_LEFT) direc = 'l';
+            if (ch == 'r' || ch == KEY_RIGHT) direc = 'r';
+            printw("THIS IS THE DIRECTION: %c", direc);
+            update_snake(&snakey, direc);
+            clear();
+            printw("location: %d %d\n", location.x, location.y);
+            printw("location: %d %d\n", snakey.positions[snakey.length - 1].x, snakey.positions[snakey.length - 1].y);
+            if (check_collision(location, snakey.positions[snakey.length - 1])){
+             printw("HEREHEREHRHERHERHEHRH!!!!");
+             tuple location = get_rand_number();
+            } 
+            print_grid(location, snakey.positions);
+            refresh();
+            usleep(500000);   // 200,000 microseconds = 0.2 seconds
         }
     }
-    print_grid(location, snakey.positions);
-    char input1;
-    scanf(" %c", &input1);
-    if (input1 == 'd'){
-        for (int i = 0 ; i < 400 ; i++){
-            if (snakey.positions[i].x == 0 && snakey.positions[i].y == 0){
-                snakey.positions[i].x = snakey.positions[i - 1].x + 1;
-                snakey.positions[i].y = snakey.positions[i - 1].y;
-             
-                break;
-            }
-            
-        }
-        for (int i = 0 ; i < 400 ; i++){
-                if (snakey.positions[i].x != -1){
-                         snakey.positions[i].x = -1;
-                         snakey.positions[i].y = -1;
-                         break;
-                    }
-            }
-    }    
 
-    print_grid(location, snakey.positions);
+    tuple get_rand_number(){
+        srand(time(NULL));
+        tuple tup = {2 + rand() % (SIZE - 2), 2 + rand() % (SIZE - 2)};
+        return tup;
+    }
 
-}
-
-tuple get_rand_number(){
-    srand(time(NULL));
-    tuple tup = {2 + rand() % (SIZE - 2), 2 + rand() % (SIZE - 2)};
-    return tup;
-}
-
-//comment 
-
-bool check_snake(int pos_x, int pos_y, tuple positions[]){
+    bool check_snake(int pos_x, int pos_y, tuple positions[]){
     for (int i = 0 ; i < SIZE * SIZE ; i++){
         if (positions[i].x == pos_x && positions[i].y == pos_y){
             return true;
-        } else {
-            continue;
         }
     }
     return false;
 }
 
-void print_grid(tuple pellet_location, tuple snake_location[]){
-    for (int i = 0 ; i < SIZE ; i++){
-            for (int z = 0 ; z < 2 * SIZE ; z++){
-                if (i != 0 && i != SIZE - 1){
-                    if (z == 0 || z == 2 * SIZE - 1){
-                        printf(".");
-                    } else {
-                        if (pellet_location.x == i && pellet_location.y == z){
-                            printf("*");
-                        } else if (check_snake(z, i, snake_location)){
-                            printf("o");
-                        } else {
-                            printf(" ");   
-                        }
-                    }
-                } else {
-                    printf(".");
+    void update_snake(snake* snakey, char direction){
+        if (direction == 'r'){
+            for (int i = 0 ; i < 400 ; i++){
+                if (snakey->positions[i].x == 0 && snakey->positions[i].y == 0){
+                    snakey->positions[i].x = snakey->positions[i - 1].x + 1;
+                    snakey->positions[i].y = snakey->positions[i - 1].y;
+                    break;
                 }
             }
-        printf("\n");
+            for (int i = 0 ; i < 400 ; i++){
+                if (snakey->positions[i].x != -1 && snakey->positions[i].y != -1){
+                    snakey->positions[i].x = -1;
+                    snakey->positions[i].y = -1;
+                    break;
+                }
+        }
+    }
+      if (direction == 'd'){
+            for (int i = 0 ; i < 400 ; i++){
+                if (snakey->positions[i].x == 0 && snakey->positions[i].y == 0){
+                    snakey->positions[i].x = snakey->positions[i - 1].x;
+                    snakey->positions[i].y = snakey->positions[i - 1].y + 1;
+                    break;
+                }
+            }
+            for (int i = 0 ; i < 400 ; i++){
+                if (snakey->positions[i].x != -1 && snakey->positions[i].y != -1){
+                    snakey->positions[i].x = -1;
+                    snakey->positions[i].y = -1;
+                    break;
+                }
+        }
+    }
+
+
+    if (direction == 'l'){
+            for (int i = 0 ; i < 400 ; i++){
+                if (snakey->positions[i].x == 0 && snakey->positions[i].y == 0){
+                    snakey->positions[i].x = snakey->positions[i - 1].x - 1;
+                    snakey->positions[i].y = snakey->positions[i - 1].y;
+                    break;
+                }
+            }
+            for (int i = 0 ; i < 400 ; i++){
+                if (snakey->positions[i].x != -1 && snakey->positions[i].y != -1){
+                    snakey->positions[i].x = -1;
+                    snakey->positions[i].y = -1;
+                    break;
+                }
+        }
+    }
+
+
+    if (direction == 'u'){
+            for (int i = 0 ; i < 400 ; i++){
+                if (snakey->positions[i].x == 0 && snakey->positions[i].y == 0){
+                    snakey->positions[i].x = snakey->positions[i - 1].x;
+                    snakey->positions[i].y = snakey->positions[i - 1].y - 1;
+                    break;
+                }
+            }
+            for (int i = 0 ; i < 400 ; i++){
+                if (snakey->positions[i].x != -1 && snakey->positions[i].y != -1){
+                    snakey->positions[i].x = -1;
+                    snakey->positions[i].y = -1;
+                    break;
+                }
+        }
     }
 }
+
+bool check_collision(tuple pellet_location, tuple snakey_head){
+    return pellet_location.x == snakey_head.x && pellet_location.y == snakey_head.y;
+}
+
+void print_grid(tuple pellet_location, tuple snake_location[]){
+        for (int i = 0 ; i < SIZE ; i++){
+                for (int z = 0 ; z < 2 * SIZE ; z++){
+                    if (i != 0 && i != SIZE - 1){
+                        if (z == 0 || z == 2 * SIZE - 1){
+                            printw(".");
+                        } else {
+                            if (pellet_location.x == i && pellet_location.y == z){
+                                printw("*");
+                            } else if (check_snake(z, i, snake_location)){
+                                printw("o");
+                            } } else if (check_snake(z, i, snake_location)){
+                                printw("o");
+                            } else {
+                                printw(" ");   
+                            }
+                        }
+                    } else {
+                        printw(".");
+                    }
+                }
+            printw("\n");
+        }
+    
 
---
+++ snake2.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <time.h>
+
+#define SIZE 20
+
+typedef struct {
+    int x;
+    int y;
+} tuple;
+
+typedef struct {
+    tuple positions[SIZE * SIZE];
+    int length;
+} snake;
+
+tuple get_random_tuple();
+snake initialize_snake();
+void draw_grid(tuple pellet, snake snakey);
+snake update_snake(char direc, snake snakey);
+char calculate_direc(int ch);
+
+tuple get_random_tuple(){
+    tuple tup = {1 + rand() % (SIZE - 1), 1 + rand() % (SIZE - 1)};
+    return tup;
+}
+
+snake initialize_snake(){
+    snake snakey;
+    snakey.length = 1;
+    snakey.positions[0].x = 1;  
+    snakey.positions[0].y = 1;
+    return snakey;
+}
+
+void draw_grid(tuple pellet, snake snakey){
+    // for (int i = 0 ; i < snakey.length ; i++){
+    //     printw("%d", snakey.positions[i].x);
+    //     printw("%d\n", snakey.positions[i].y);
+    // }
+    mvaddch(pellet.y, pellet.x, '#');
+    for (int i = 0 ; i < snakey.length ; i++){
+        mvaddch(snakey.positions[i].y, snakey.positions[i].x, 'o');
+    }
+    for (int i = 0 ; i < SIZE ; i++){
+        for (int z = 0 ; z < SIZE ; z++){
+            if (i == 0 || i == SIZE - 1 ){
+                mvaddch(i, z, '-');
+            } else if (z == 0 || z == SIZE - 1 ){
+                mvaddch(i, z, '|');
+            }
+        }
+    }
+    refresh();
+}
+
+snake update_snake(char direc, snake snakey){
+    tuple new_positions[SIZE * SIZE];
+    int ch = -1;
+
+    for (int i = 0 ; i < snakey.length ; i++){
+         printw("%dSSS", snakey.positions[i].x);
+         printw("%d\n", snakey.positions[i].y);
+    }
+    
+    if (direc == 'd'){
+        new_positions[0].x = snakey.positions[0].x;
+        new_positions[0].y = snakey.positions[0].y + 1;
+    }
+    else if (direc == 'u'){
+        new_positions[0].x = snakey.positions[0].x;
+        new_positions[0].y = snakey.positions[0].y - 1;
+    }
+    else if (direc == 'l'){
+        new_positions[0].x = snakey.positions[0].x - 1;
+        new_positions[0].y = snakey.positions[0].y;
+    }
+    else if (direc == 'r'){
+        new_positions[0].x = snakey.positions[0].x + 1;
+        new_positions[0].y = snakey.positions[0].y;
+    }
+
+    for (int i = 0 ; i + 1 < snakey.length ; i++){
+        new_positions[i + 1].x = snakey.positions[i].x;
+        new_positions[i + 1].y = snakey.positions[i].y;
+    }
+     
+    for (int i = 0 ; i < snakey.length ; i++){
+        snakey.positions[i].x = new_positions[i].x;
+        snakey.positions[i].y = new_positions[i].y;
+    }
+
+    return snakey;
+}
+
+ char calculate_direc(int ch) {
+    if (ch == 'd' || ch == KEY_DOWN) return 'd';
+    else if (ch == 'u' || ch == KEY_UP) return 'u';
+    else if (ch == 'l' || ch == KEY_LEFT) return 'l';
+    else if (ch == 'r' || ch == KEY_RIGHT) return 'r';
+    return 'q';
+}
+
+
+int main (void){
+    initscr();
+    nodelay(stdscr, TRUE);
+    keypad(stdscr, TRUE);
+    srand(time(NULL));
+    tuple pellet = get_random_tuple();
+    snake snakey = initialize_snake();
+    char direc = 'd';
+    while (true) {
+        clear();
+        int ch = getch();
+        if (ch != ERR){
+            direc = calculate_direc(ch);
+        }
+        snakey = update_snake(direc, snakey);
+        draw_grid(pellet, snakey);
+        napms(400);
+    }
+    endwin();
+}
+
+
+  
+        
